49a9f8696bd34153610f8980ca47d0e16b9ee0f5
Nathaniel Misawo <nathaniel.misawo@strathmore.edu>
2026-06-19 20:21:39 +0300

    Creating Java Script

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
+function validateForm() {
+    let name = document.getElementById('name').value;
+    if (name ==""){
+        alert("Please enter your name.");
+        return false;
+    }
+    return true;
+}
+
+function filterProducts() {
+    let input = document.getElementById('searchInput').value.toLowerCase();
+    let items = document.querySelectorAll('.product-item');
+
+    items.forEach(item => {
+       item.style.display=item.textContent.toLowerCase().includes(input)?"":"none";
+});
+}
